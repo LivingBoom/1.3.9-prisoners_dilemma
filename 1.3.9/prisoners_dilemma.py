a123d7bce00cc56825a918268a5c0264853b37c2
jlivstone <60622929+jlivstone@users.noreply.github.com>
2020-02-12 13:13:51 -0500

    x

--- 1.3.9/prisoners_dilemma.py
+++ 1.3.9/prisoners_dilemma.py
@@ -378,5 +378,5 @@ def post_to_file(string, filename='tournament.txt', directory=''):
  
 ### Call main_play() if this file is executed
 if __name__ == '__main__':
-    scores, moves, reports = main_play(modules[0:4])   
+    scores, moves, reports = main_play(modules[0:20])   
     section0, section1, section2, section3 = reports
