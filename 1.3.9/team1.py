1a019b132aed83b0b9f4f8a3c337c7caec00a5a0
LemonadeBread <60623010+LemonadeBread@users.noreply.github.com>
2020-02-12 13:22:40 -0500

    Update team1.py

--- 1.3.9/team1.py
+++ 1.3.9/team1.py
@@ -26,7 +26,12 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    import random
+    a = rand.int(1,100)
+    if a < 50
+        return 'c'
+    else
+        return 'b'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
