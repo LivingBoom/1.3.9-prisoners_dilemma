d0a867bc4c4b5e4b1f2791662016e5c49d14a653
LemonadeBread <60623010+LemonadeBread@users.noreply.github.com>
2020-02-12 14:27:04 -0500

    yhg

--- 1.3.9/team4.py
+++ 1.3.9/team4.py
@@ -26,7 +26,10 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    if (their_history[-1] == 'b'):
+        return 'b'
+    else:
+        return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
